8f2fcf7973d92058b9569cc25b6993aaa0817703
Ian Maurer <ian@genomoncology.com>
2018-02-13 07:33:05 -0500

    Issue #9 - Keep the null/None value if that is what was provided.

--- tests/issues/test_issue_009.py
+++ tests/issues/test_issue_009.py
@@ -18,11 +18,16 @@ def test_empty_mapping():
     assert related.to_yaml(root).strip() == OUTPUT_YAML
 
 
+def test_null_mapping():
+    root = related.from_yaml(OUTPUT_YAML, Root)
+    assert related.to_yaml(root).strip() == OUTPUT_YAML
+
+
 def test_invalid_mapping():
     with pytest.raises(TypeError):
         related.from_yaml(INVALID_YAML, Root)
 
 
 INPUT_YAML = "children:"
-OUTPUT_YAML = "children: {}"
+OUTPUT_YAML = "children: null"
 INVALID_YAML = "children: 5"
